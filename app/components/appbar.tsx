7bf43314eeca81c391fe1e89c92a127f2a512412
Gagan Arora <gaganarora1.2005@gmail.com>
2026-02-17 21:03:51 +0530

    Adding git login/Prisma/Testing

--- app/components/appbar.tsx
+++ app/components/appbar.tsx
@@ -7,6 +7,9 @@ export default function Appbar(){
 
     return <div>
         {session.data?.user && <button className="m-2 p-2 bg-blue-400"  onClick={()=>signOut()}>Logout</button>}
-        {!session.data?.user && <button className="m-2 p-2 bg-blue-400" onClick={()=>signIn()}>Signin</button>}
+        
+        {!session.data?.user && <div><div>Sign in With Google</div> <button className="m-2 p-2 bg-blue-400" onClick={()=>signIn("google")}>Signin</button></div>}
+        
+        {!session.data?.user && <div><div>Sign in with Github</div><button className="m-2 p-2 bg-blue-400" onClick={()=>signIn("github")}>Signin</button></div>}
     </div>
 }
