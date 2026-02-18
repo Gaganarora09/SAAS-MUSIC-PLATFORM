1e8ce1696d98112c2d96568f53291299a281966e
Gagan Arora <gaganarora1.2005@gmail.com>
2026-02-19 01:48:55 +0530

    Almost all necessary end points are added

--- app/api/Streams/route.ts
+++ app/api/Streams/route.ts
@@ -42,3 +42,18 @@ export async function POST(req:NextRequest){
         })
     }
 } 
+
+//To fetch all the streams
+
+export async function Get(req:NextRequest){
+    const creatorId =req.nextUrl.searchParams.get("creatorId");
+    const streams=await prismaClient.stream.findMany({
+        where:{
+            userId:creatorId??""
+        }
+    })
+
+    return NextResponse.json({
+        streams
+    })
+}
