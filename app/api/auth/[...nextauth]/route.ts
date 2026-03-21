a6512ba1b9c0b392aa07b0b37b2da7844d2c846b
Gagan Arora <gaganarora1.2005@gmail.com>
2026-03-21 14:32:52 +0530

    adding secret nectauth to [...nextauth]

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -14,6 +14,7 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GITHUB_SECRET ?? ""
     }),
   ],
+    secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
  async signIn(params) {
   if (!params.user.email) return false;
